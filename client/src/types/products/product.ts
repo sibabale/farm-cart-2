699a1085ecdf0db6cec0cff1d4e03260d80ec033
sibabale <22472288+zbisj@users.noreply.github.com>
2023-07-10 21:09:36 +0200

    refactor: extending the product type

--- client/src/types/products/product.ts
+++ client/src/types/products/product.ts
@@ -1,16 +1,19 @@
-import { UserType } from './../users/users';
 
 export type ProductType = {
-  owner: Object | UserType,
   stud: boolean | null
+  city: string
+  owner: string,
   breed: string
   price: number
   title: string
   images:string[]
   weight: number
   sold_at: string
+  province: string
+  zip_code: number
   quantity: number
   description: string
   sub_category: string
   main_category: string
+  street_address: string
 }
