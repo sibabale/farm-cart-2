1f013ce321f876b4c9f813a2c6afe48e526914b6
sibabale <22472288+zbisj@users.noreply.github.com>
2023-07-10 19:00:52 +0200

    refactor: extending types for atomic components

--- client/src/types/props/atoms.ts
+++ client/src/types/props/atoms.ts
@@ -4,7 +4,8 @@ export type ButtonProps = {
   as?: string,
   bg?: string, 
   to?: string, 
-  size?: string, 
+  size?: 'lg' | 'md' | 'sm'
+  type?: 'submit' | 'reset' | 'button' | undefined
   text?: string, 
   icon?: string, 
   color?: string, 
@@ -19,6 +20,7 @@ export type ButtonProps = {
 export type InputProps = {
   name?: string, 
   type?: string, 
+  required: boolean, 
   onChange?: (event: any) => void, 
   className: string, 
   placeholder: string, 
@@ -27,7 +29,17 @@ export type InputProps = {
 export type SelectProps = {
   name?: string, 
   items: string[] | [], 
-  className: string, 
-  placeholder: string, 
+  required: boolean
+  className: string
+  placeholder: string
   onSelectChange: (arg0: string) => void | []
+}
+
+export type ImageUploaderProps = {
+  id: string, 
+  name?: string, 
+  image: string, 
+  required?: boolean, 
+  className: string, 
+  onChange: (arg:any) => void, 
 }
