2372967766446d93d26680af0f365b47a588501a
sibabale <22472288+zbisj@users.noreply.github.com>
2023-07-10 19:03:05 +0200

    refactor: getting the redux state of the new product

--- client/src/redux/products/product.selector.js
+++ client/src/redux/products/product.selector.js
@@ -1,7 +1,8 @@
 // The function below is called a selector and allows us to select a value from
 // the state. Selectors can also be defined inline where they're used instead of
 // in the slice file. For example: `useSelector((state) => state.counter.value)`
-export const selectCount = (state) => state.new_product.value
+export const selectProduct = (state) => state.new_product
+
 export const selectSubCategory = (state) => state.new_product.sub_category;
 export const selectMainCategory = (state) => state.new_product.main_category;
 
