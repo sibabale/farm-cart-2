d9f73674e97d28ee48a206b7846207530064a318
sibabale <22472288+zbisj@users.noreply.github.com>
2023-07-11 22:36:40 +0200

    refactor: rendering images from the back end on the image grid

--- client/src/components/organisms/image_grid/image_grid.tsx
+++ client/src/components/organisms/image_grid/image_grid.tsx
@@ -5,7 +5,7 @@ import {Grid, SeeAll,GridContainer, MobileImage} from './image_grid.styles';
 type ImageGridProps = {
   images: string[]
 }
-export const ImageGrid = ({images}: ImageGridProps) => {
+export const ImageGrid = ({images}: ImageGridProps) => {  
   return(
     <>
     <MobileImage>
@@ -14,12 +14,11 @@ export const ImageGrid = ({images}: ImageGridProps) => {
     </MobileImage>
     <GridContainer>
       <Grid>
-        <div style={{backgroundImage: 'url(../images/cows/cow-0.jpeg)'}} className='item1' />
-        <div style={{backgroundImage: 'url(../images/cows/cow-0.jpeg)'}} className='item2' />
-        <div style={{backgroundImage: 'url(../images/cows/cow-0.jpeg)'}} className='item2' />
-        <div style={{backgroundImage: 'url(../images/cows/cow-0.jpeg)'}} className='item3' />
-        <div style={{backgroundImage: 'url(../images/cows/cow-0.jpeg)'}} className='item4' />
-        <div className='item5' style={{backgroundImage: 'url(../images/cows/cow-0.jpeg)'}} > 
+        <div style={{backgroundImage: `url(${images[0]})`}} className='item1' />
+        <div style={{backgroundImage: `url(${images[1]})`}} className='item2' />
+        <div style={{backgroundImage: `url(${images[2]})`}} className='item3' />
+        <div style={{backgroundImage: `url(${images[3]})`}} className='item4' />
+        <div className='item5' style={{backgroundImage: `url(${images[4]})`}} > 
           <SeeAll>See All Photos</SeeAll>
         </div>
       </Grid>
