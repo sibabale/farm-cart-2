e14e4c87b86d0e2db2502eedd009b05f9fb76584
sibabale <22472288+zbisj@users.noreply.github.com>
2023-07-11 22:37:12 +0200

    refactor: update the product cta to showing banking details

--- client/src/components/organisms/product_cta/product_cta.styles.ts
+++ client/src/components/organisms/product_cta/product_cta.styles.ts
@@ -2,16 +2,27 @@ import styled from 'styled-components';
 
 export const ProductCTAContainer = styled.div`
   width: 400px;
-  height: 200px;
+  height: fit-content;
   border: 2px solid lightgray;
   display: flex;
-  padding: 40px;
+  padding: 20px;
   border-radius: 10px;
   flex-direction: column;
   justify-content: space-around;
+
+  button {
+    margin: 20px 0;
+  }
 `;
 
 export const Price = styled.span`
   font-size: 22px;
   font-weight: 800;
+`;
+
+export const BankingDetails = styled.div`
+  padding: 10px;
+  margin-top: 20px;
+  border-radius: 10px;
+  background-color: #d9d9d9;
 `;
--- client/src/components/organisms/product_cta/product_cta.tsx
+++ client/src/components/organisms/product_cta/product_cta.tsx
@@ -1,16 +1,47 @@
-
+import {useState} from 'react';
 import { Button } from "../../atoms/button/button";
-import {Price, ProductCTAContainer} from "./product_cta.styles"
+import {
+  Price, 
+  BankingDetails,
+  ProductCTAContainer
+} from "./product_cta.styles"
 
 export interface IProductCTAProps {
   price: number,
 }
 
 export default function ProductCTA ({price}: IProductCTAProps) {
+
+  const [isCheckingOut, setIsCheckingOut] = useState(false)
   return (
     <ProductCTAContainer>
       <Price>R {price}</Price>
-      <Button text="Chat to Seller" icon="../images/whatsapp.png"/> 
+      <Button 
+        bg="black"
+        text={isCheckingOut ? 'Hide Bank Details' : "Checkout"}
+        color="white"
+        isRound
+        onClick={() => setIsCheckingOut(!isCheckingOut)}
+        /> 
+      {
+        isCheckingOut && (
+          <>
+            <small>
+              <b>
+                <u>NB:</u> Make your payment to the following account and keep your
+                  Reference Number safe.
+                  Once you funds reflect the seller will contact you to make arrangements.
+              </b>
+            </small>
+            <BankingDetails>
+              <p><b>Bank:</b> Absa</p>
+              <p><b>Branch Code:</b> 0000</p>
+              <p><b>Account Number:</b> 0000 000 000 000</p>
+              <p><b>Reference Number:</b> 00000 0000 0000</p>
+            </BankingDetails>
+          </>
+        )
+      }
     </ProductCTAContainer>
   );
 }
