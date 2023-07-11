540fb892ef7893a70cf431e87385c409ccf0f721
sibabale <22472288+zbisj@users.noreply.github.com>
2023-07-11 22:35:58 +0200

    style: increasing padding

--- client/src/components/atoms/badge/badge.styles.ts
+++ client/src/components/atoms/badge/badge.styles.ts
@@ -5,7 +5,7 @@ export const BadgeContainer = styled.span`
   color: white;
   height: 5px;
   display: flex;
-  padding: 10px;
+  padding: 11px;
   background: rgb(0,0,0);
   background: linear-gradient(69deg, rgba(0,0,0,0.6265099789915967) 100%, rgba(255,255,255,0) 100%);
   align-items: center;
