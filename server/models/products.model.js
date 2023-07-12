73553dc8013ffbf2a6c0f66c735096055a17b1f6
sibabale <22472288+zbisj@users.noreply.github.com>
2023-07-12 22:57:33 +0200

    refactor: instantiating product data with data in a json file

--- server/models/products.model.js
+++ server/models/products.model.js
@@ -1,70 +1,34 @@
 var moment = require('moment');
 const { v4: uuidv4 } = require('uuid');
-// import { uid } from 'uid';
+const Products = require('../data/products.json');
 
-const product = {
-  "id": 1234423432,
-  "owner": {
-    "uid": "121212121232",
-    "verified": false,
-    "personal_information": {
-      "image": "https://shorturl.at/cjlG2",
-      "email": "jd@email.com",
-      "last_name": "Doe",
-      "first_name": "John",
-      "cell_number": "081 317 0529"
-    },
-    "location": {
-      "city": "Carmineton",
-      "country": "Elnahaven",
-      "zip_code": "87617",
-      "latitude": "-58.3066",
-      "longitude": "-12.0041"
-    }
-  },
-  "breed": "Boran",
-  "price": 50000,
-  "title": "Boran Bull",
-  "images": [
-    "https://bit.ly/41C5Ri9", 
-    "https://bit.ly/3or8DIH", 
-    'https://shorturl.at/euwHK',
-    "https://shorturl.at/sUYZ6",
-    "https://shorturl.at/rvO39"
-  ],
-  "weight": 200,
-  "sold_at": null,
-  "created_at": "20 Jan 2023",
-  "category": {
-    "main": "animals",
-    "sub": "cows"
-  },
-  "quantity": 3,
-  "description": "Fugiat quis et eu esse reprehenderit pariatur dolore commodo cillum.d",
-};
-
-const products = new Map();
-
-products.set(product.id, product);
+const products = [...Products];
 
 function addNewProduct(product) {
   const id =  uuidv4();
-
-  return products.set(
-    id,
-    Object.assign(product, {
-      id, 
-      created_at: moment().format()
-    })
-  );
-  
+  return products.push({...product, id, created_at: moment().format()});
 };
 
 function getAllProducts() {
-  return Array.from(products.values());
+  return products;
+};
+
+function getProductById(productId) {
+  return products.find(item =>  item.id === Number(productId));
+};
+
+function getProductsBySubCategory(subCategory) {
+  return products.filter(item => item.sub_category === subCategory);
+};
+
+function getProductsByMainCategory(mainCategory) {
+  return products.filter(item => item.main_category === mainCategory)
 };
 
 module.exports = {
   addNewProduct,
-  getAllProducts
+  getProductById,
+  getAllProducts,
+  getProductsBySubCategory,
+  getProductsByMainCategory
 };
