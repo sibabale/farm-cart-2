0ac97b8bcb77d1bf67249e7e6fcc74d39afad803
sibabale <22472288+zbisj@users.noreply.github.com>
2023-08-02 23:48:05 +0200

    refactor: copy update

--- client/src/components/atoms/empty_state/empty_state.tsx
+++ client/src/components/atoms/empty_state/empty_state.tsx
@@ -12,7 +12,7 @@ export default function EmptyState () {
   return (
     <EmptyStateContainer >
       <h2>Sorry we don’t have this product at the moment</h2>
-      <h3>If you it available for sale, contact us below</h3>
+      <h3>If you have it available for sale, contact us below</h3>
       <WhatsappLink href="https://wa.me/27813170529?text=Hi I would like to sell">
         
         <img src="../../images/icons/social/whatsapp.svg" alt="Whatsapp icon" />
